4adfe4b995c7baa24db2730f36bd0b0ca71ef1d1
mac-hart <hartsellmac@gmail.com>
2024-03-07 20:21:41 -0700

    final touches for real

--- src/Heading.tsx
+++ src/Heading.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Heading.css';
 
 function Heading() {
   return (
     <div>
-      <h1>IT'S MARCH, BABY.</h1>
+      <img src="march.png" alt="March Madness" />
+      <h1 style={{ color: 'royalblue' }}>IT'S MARCH, BABY.</h1>
+      <img
+        src="/ball.webp"
+        alt="basketball"
+        className="rotating-image"
+        style={{ width: '100px', height: '100px' }}
+      />
       <h3>Let the Madness ensue.</h3>
       <h5>
         The NCAA Men's Basketball March Madness tournament is an annual college
@@ -14,6 +22,7 @@ function Heading() {
         for its thrilling single-elimination format, unpredictable upsets, and
         intense competition, captivating basketball fans worldwide.
       </h5>
+      <h3>Here is a list of all Division 1 teams:</h3>
     </div>
   );
 }
